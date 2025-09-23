b17d8f8ffd464fbe66ba17d239aa7d1763b90b3b
Lalit Mehra <lokeshmehra7039@gmail.com>
2025-09-23 11:50:47 +0530

    handle post and update requests if cliend send the invalid data from the Hoppscotch or postman,etc. and also wrap all async function inside the wrapAync which helps to reduce the bulky way to write try-catch block

--- app.js
+++ app.js
@@ -37,10 +37,10 @@ app.get('/',(req,res)=>{
 });
 
 // index route 
-app.get('/listings',async(req,res)=>{
+app.get('/listings',wrapAsync(async(req,res)=>{
     const allListing= await Listing.find({});
     res.render("listings/index.ejs",{allListing})
-});
+}));
 
 // New Route 
 app.get('/listings/new',(req,res)=>{
@@ -48,40 +48,46 @@ app.get('/listings/new',(req,res)=>{
 })
 
 // show Route 
-app.get('/listings/:id',async(req,res)=>{
+app.get('/listings/:id',wrapAsync(async(req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing})
-})
+}));
 
 // Create Route 
 app.post('/listings',wrapAsync(async (req,res,next)=>{
-        // let {title,description,image,price,country,location} = req.body;
-        const newListing = new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect('/listings');
+     // let {title,description,image,price,country,location} = req.body;
+     if(!req.body.listing){
+        throw new ExpressError(404,"Send valid data for listing")
+     }
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect('/listings');
 }));
 
 // Edit Route 
-app.get('/listings/:id/edit',async(req,res)=>{
+app.get('/listings/:id/edit',wrapAsync(async(req,res)=>{
     let{id} = req.params;
     const listing = await Listing.findById(id);
     res.render('listings/edit.ejs',{listing});
-})
+}));
 
 // Update Route 
-app.put('/listings/:id',async(req,res)=>{
+app.put('/listings/:id',wrapAsync(async(req,res)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"Send valid data for listing");
+    }
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing})
     res.redirect(`/listings/${id}`);
-})
+}));
 
 // Delete route 
-app.delete('/listings/:id',async (req,res)=>{
+app.delete('/listings/:id',wrapAsync(async (req,res)=>{
     let {id} = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     res.redirect('/listings')
-})
+}));
 
 // app.get('/testListing',async(req,res)=>{
 //     let sampleListing = new Listing({
@@ -97,9 +103,13 @@ app.delete('/listings/:id',async (req,res)=>{
 //     res.send("succesfully saved")
 // });
 
+app.all(/.*/,(req,res,next)=>{
+    next(new ExpressError(404,"Path Not Found!"));
+})
+
 app.use((err,req,res,next)=>{
-    let {statusCode,message} = err;
-    res.atatus(statusCode).send(message);
+    let {statusCode = 500,message = "Something went wrong"} = err;
+    res.status(statusCode).send(message);
 })
 
 app.listen(port,()=>{
